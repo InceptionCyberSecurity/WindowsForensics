babc2c47ffa0c8a03ddc0afb3b31be89d7deda59
Arcadeus OPS <info@arcadeusops.com>
2023-02-28 17:15:50 +0800

    Update winfor.ps1

--- winfor.ps1
+++ winfor.ps1
@@ -1,7 +1,5 @@
-get computer info
- Get-CimInstance -Class CIM_ComputerSystem -ComputerName localhost -ErrorAction Stop | Select-Object *
+# get computer info
+Get-CimInstance -Class CIM_ComputerSystem -ComputerName localhost -ErrorAction Stop | Select-Object *
 
- get remote system info
- Get-CimInstance -Class CIM_ComputerSystem -ComputerName (Get-Content -Path C:\Temp\Servers.txt) -ErrorAction Stop | Select-Object * | Out-GridView
-
- 
+# get remote system info
+Get-CimInstance -Class CIM_ComputerSystem -ComputerName (Get-Content -Path C:\Temp\Servers.txt) -ErrorAction Stop | Select-Object * | Out-GridView
